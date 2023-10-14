06315180381a390a1ca35986e1081b99e11395da
OITD <oitd@banxico.org.mx>
2023-10-14 12:57:39 -0600

    Initial Commit

---
+++ Posts CRUD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posts CRUD
+//
+//  Created by OITD on 14/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
